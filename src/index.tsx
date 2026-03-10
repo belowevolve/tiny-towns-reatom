7e9a8bb661cef0cb18dc40e58859c21d089bc9cd
belowevolve <130287859+belowevolve@users.noreply.github.com>
2026-03-10 22:49:28 +0300

    added polyfill for interestfor

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import { mount } from "@reatom/jsx";
 
 import { App } from "./app";
 
+if (!("interestForElement" in HTMLButtonElement.prototype)) {
+  import("interestfor");
+}
+
 if (import.meta.env.MODE === "development") {
   connectLogger();
 }
